2a012d9399de82b76f159dcfaca290c2dc83caca
DMMaheen <90624217+DMMaheen@users.noreply.github.com>
2021-09-30 13:39:51 +0530

    update

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,27 @@
+import { Row, Col, Form, Button } from "react-bootstrap";
+import "./Feedback.css";
+export const Feedback = () => {
+  return (
+    <Form>
+      <Form.Control
+        placeholder="Enter The Title"
+        className="fbrow"
+      ></Form.Control>
+      <Form.Control
+        as="textarea"
+        placeholder="Enter Feedback Here"
+        className="fbrow"
+      ></Form.Control>
+      <Row className="fbrow">
+        <Col lg={8}></Col>
+        <Col lg={1}>
+          <Button>Submit</Button>
+        </Col>
+        <Col lg={1}>
+          <Button>Cancel</Button>
+        </Col>
+        <Col lg={2}></Col>
+      </Row>
+    </Form>
+  );
+};
